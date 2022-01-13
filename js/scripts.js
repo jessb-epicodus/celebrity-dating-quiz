53de3009e368ad9ef7bd72b8da6c3285acfd15b8
jb <jessb.epicodus@gmail.com>
2022-01-13 13:07:12 -0800

    add working submit button & return match based on input

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    const age = parseInt($("input#age").val());
+    const gender = $("select#gender").val();
+    $("#austin").hide();
+    $("#minion").hide();
+
+    if ((gender === "female" || "non-binary" || "male") && age >= 30) {
+    $("#austin").show();
+    } else if ((gender === "female" || "non-binary" || "male") && age < 30) {
+    $("#minion").show();
+    }
+    event.preventDefault();
+  });
+});
